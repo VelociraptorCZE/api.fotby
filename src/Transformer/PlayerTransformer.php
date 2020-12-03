67eb0e370355b514e32d3b28b4b044501806e7ce
Simon Raichl <raichls44@gmail.com>
2020-12-03 02:16:59 +0100

    Exception in PlayerTransformer is thrown when player account is null

--- src/Transformer/PlayerTransformer.php
+++ src/Transformer/PlayerTransformer.php
@@ -4,16 +4,23 @@ declare(strict_types=1);
 namespace App\Transformer;
 
 use App\Entity\Player;
+use InvalidArgumentException;
 
 class PlayerTransformer
 {
     public function transform(Player $player): array
     {
+        $playerAccount = $player->getPlayerAccount();
+
+        if ($playerAccount === null) {
+            throw new InvalidArgumentException('Could not get player data properly');
+        }
+
         return [
             'createdAt' => $player->getCreatedAt()->format('j. n. Y'),
-            'balance' => $player->getPlayerAccount()->getBalance(),
-            'equippedSkin' => $player->getPlayerAccount()->getEquippedSkin(),
-            'skins' => $player->getPlayerAccount()->getSkins()
+            'balance' => $playerAccount->getBalance(),
+            'equippedSkin' => $playerAccount->getEquippedSkin(),
+            'skins' => $playerAccount->getSkins()
         ];
     }
 }
